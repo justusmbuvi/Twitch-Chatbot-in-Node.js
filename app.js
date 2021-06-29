8cb3fa4d9feb60fe1aa1ea49d456601f0637d8ba
Justus Mbuvi <82378920+justusmbuvi@users.noreply.github.com>
2021-06-29 04:33:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
+// Require necessary node modules
 // Make the variables inside the .env element available to our Node project
 require('dotenv').config();
 
 const tmi = require('tmi.js');
+
+// Setup connection configurations
+// These include the channel, username and password
 const client = new tmi.Client({
     options: { debug: true, messagesLogLevel: "info" },
     connection: {
         reconnect: true,
         secure: true
     },
+
+    // Lack of the identity tags makes the bot anonymous and able to fetch messages from the channel
+    // for reading, supervison, spying or viewing purposes only
     identity: {
         username: `${process.env.TWITCH_USERNAME}`,
         password: `oauth:${process.env.TWITCH_OAUTH}`
     },
     channels: [`${process.env.TWITCH_CHANNEL}`]
 });
+
+// Connect to the channel specified using the setings found in the configurations
+// Any error found shall be logged out in the console
 client.connect().catch(console.error);
+
 client.on('message', (channel, tags, message, self) => {
+    // Lack of this statement or it's inverse (!self) will make it in active
     if (self) return;
+
+    // Create up a switch statement with some possible commands and their outputs
+    // The input shall be converted to lowercase form first
+    // The outputs shall be in the chats
     switch (message.toLowerCase()) {
+        // Use tags to obtain the username of the one who has keyed in a certain input
         case 'commands':
             client.say(channel, `@${tags.username}, available commands are:
             Commands Help Greetings Hi !Website !Name
@@ -35,10 +52,7 @@ client.on('message', (channel, tags, message, self) => {
             client.say(channel, `${tags.username}, hola!`);
             break;
         case '!name':
-            client.say(channel, `Hello @${tags.username}, my name is ChatBot! Press "1" to continue...`);
-            if (message.toLowerCase() === '1') {
-                client.say(channel, `I was built today!`);
-            }
+            client.say(channel, `Hello @${tags.username}, my name is ChatBot! Type "help" to continue...`);
             break;
         case 'help':
             client.say(channel, `${tags.username}, Use the following commands to get quick help:
@@ -47,10 +61,26 @@ client.on('message', (channel, tags, message, self) => {
             Greetings: Get Greetings || 
             Hi: Get "Hola" || 
             !Website: Get my website || 
-            !Name: Get my name  
-    
+            !Name: Get my name || 
+            !Upvote first_name second_name: Upvote user first_name second_name ||  Upvote first_name second_name: Upvote user first_name second_name || 
+            !Cheer first_name second_name: Cheer first_name second_name || Cheers first_name second_name: Cheer first_name second_name --
+
             For more help just ping me up!
             `);
+            break;
+        default:
+
+            // We shall convert the message into a string in which we shall check for its first word
+            // and use the others for output
+            let mymessage = message.toString();
+            if ((mymessage.split(' ')[0]).toLowerCase() === '!upvote' || 'upvote') {
+                // You can add some emojis which will appear in the chat using their emoji names
+                // For example "PopCorn"
+                client.say(channel, `TwitchLit @${(mymessage.split(' ')[1] + '_' + mymessage.split(' ')[2])} TwitchLit  you have been UPVOTED by ${ tags.username }`);
+
+            } else if ((mymessage.split(' ')[0]).toLowerCase() === '!cheer' || 'cheers') {
+                console.log(`HSCheers @${(mymessage.split(' ')[1] + '_' + mymessage.split(' ')[2])} HSCheers you have been UPVOTED by ${ tags.username }`);
+            }
             break;
     }
 });
